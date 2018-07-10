49e52dc8b2c68481339e49b68230acf0d62ad3de
easy <easy@desk>
2018-07-10 12:19:54 +0900

    BTest 두번째 커밋 : B사용자 출력문 추가~~

--- JAVA_Git/src/BTest.java
+++ JAVA_Git/src/BTest.java
@@ -1,7 +1,8 @@
 
 public class BTest {
 	public static void main(String[] args) {
-		System.out.println("BTest 첫번째 커밋 ~~");
-		System.out.println("BTest 두번째 커밋 : A사용자 ~~");
+		System.out.println("BTest 泥ル쾲吏� 而ㅻ컠 ~~");
+		System.out.println("BTest �몢踰덉㎏ 而ㅻ컠 : A�궗�슜�옄 ~~");
+		System.out.println("BTest 두번째 커밋 : B사용자 출력문 추가~~");
 	}
 }
